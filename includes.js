2d425403295ff1a34ab9c5212ca300e9f2f51aff
Marius <mariuslangeland.ml@gmail.com>
2022-05-21 00:29:58 +0200

    vcb vf

--- includes.js
+++ includes.js
@@ -1,3 +1,11 @@
-$(function(){$("#footer").load("footer.html");});
-$(function(){$("#navbar").load("navbar.html");});
-$(function(){$(".menu-icon").load("objects.html")})
+var htmlObjects;
+$(function(){
+    fetch('footer.html').then(response => response.text().then(html => $("#footer").append(html)));
+    fetch('navbar.html').then(response => response.text().then(html => $("#navbar").append(html)));
+    fetch('objects.html').then(response => response.text().then(html => htmlObjects = html));
+    
+    $.get("objects.html", data => {
+        data = $.parseHTML(data);
+        $(".create-menu-icon").append();
+    });
+});
