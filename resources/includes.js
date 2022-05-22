4ff8593912ab8d1950897924fcd847cf2c7a0dab
Marius-Langeland <mariuslangeland.ml@gmail.com>
2022-05-22 12:07:59 +0200

    nkj

--- resources/includes.js
+++ resources/includes.js
@@ -1,13 +1,9 @@
 $(function(){
-    fetch('resources/footer.html').then(response => response.text().then(html => $("#footer").append(html)));
-    fetch('resources/navbar.html').then(response => response.text().then(html => $("#navbar").append(html)));
-    fetch('resources/objects.html').then(response => response.text().then(html => htmlObjects = html));
-    
     $.get("resources/objects.html", data => {
-        $("*[data-object]").each(function(){
-            let object = $(this).data("object");
-            console.log(object);
-            $(this).append($(`#${object}`, data).html());
+        $("*[data-object]").each((index, obj) => {
+            let object = $(obj).data("object");
+            let h = $(data).filter(`.${object}`);
+            $(obj).append(h);
         });
     });
 });
