c2365f366abe4a4e934fe1ab4132cfad843335e8
Marius-Langeland <mariuslangeland.ml@gmail.com>
2022-05-24 13:08:09 +0200

    Fixed undefined image source

--- resources/includes.js
+++ resources/includes.js
@@ -16,11 +16,12 @@ $(function(){
         $.get(`${data}`, get =>{
             let title = $(get).find("#title").text();
             let bg = $(get).find("#background").attr("src");
+            bg = bg == undefined ? "" : bg;
             let desc = $(get).find("#desc").text();
             let node = `<h3>${title}</h3>
                         <img src="${bg}">
-                        <p>${desc}</p>`
-
+                        <p>${desc}</p>`;
+            
             $(obj).append(node);
         });
 
