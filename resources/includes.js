2bc4a668c4dac90ec8ecb70a3b4a4f688c0710f1
Marius-Langeland <mariuslangeland.ml@gmail.com>
2022-05-24 10:55:41 +0200

    removed footer

--- resources/includes.js
+++ resources/includes.js
@@ -1,6 +1,6 @@
 $(function(){
     $.get("resources/objects.html", data => {
-        $("*[data-object]").each((index, obj) => {
+        $("[data-object]").each((index, obj) => {
             let object = $(obj).data("object");
             let h = $(data).filter(`.${object}`);
             $(obj).append(h);
@@ -12,6 +12,16 @@ $(function(){
     });
 
     $("[data-link]").each((index, obj) =>{
-        
+        let data = $(obj).data("link");
+        $.get(`projects/${data}`, get =>{
+            var x = $("html").find("#");
+            alert(x);
+
+            obj.append(`
+                <h3>${title}</h3>
+                ${background}
+                <p>${text}</p>
+            `);
+        });
     });
 });
